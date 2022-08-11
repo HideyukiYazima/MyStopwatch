9894f0f3d1043428030afff401cf225b2642ecfc
Yazima <yazima04200@gmail.com>
2022-08-11 11:31:26 +0900

    タイマーstart実装

--- js/main.js
+++ js/main.js
@@ -5,4 +5,26 @@
   const start = document.getElementById('start');
   const stop = document.getElementById('stop');
   const reset = document.getElementById('reset');
+
+  let startTime;
+
+  // タイマーの動きや描画の設定
+  function countUp() {
+    console.log(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
+    timer.textContent = `${m}:${s}.${ms}`;
+
+    setTimeout(() => {
+      countUp();
+    }, 10);
+  }
+  //
+
+  start.addEventListener('click', () => { // クリックしたときの処理
+    startTime = Date.now();
+    countUp();
+  });
 }
